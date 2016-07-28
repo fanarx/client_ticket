28b02e68fe63fdc76c216e360114690f3e7703a1
Suren <fanarx@gmail.com>
2016-07-29 00:52:14 +0400

    local config

--- webpack.config.js
+++ webpack.config.js
@@ -3,20 +3,21 @@ var path = require('path');
 module.exports = {
   entry: {
     create_ticket: './src/create_ticket_app.js',
-    search_grid: './src/search_grid_app.js',
-    success_message: './src/success_message_app.js'
+    // search_grid: './src/search_grid_app.js',
+    // success_message: './src/success_message_app.js'
   },
   output: {
     path: path.join(__dirname, 'js'),
-    filename: "[name].bundle.js"
+    filename: "[name].bundle.js",
+    publicPath: 'js'
   },
   //devtool: 'cheap-module-source-map',
-  devtool: 'source-map',
+  devtool: 'eval',
   module: {
     loaders: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
+        include: /src/,
         loaders: ['babel-loader']
       }
     ]
